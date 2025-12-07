997552b879bc6718c06bb00f1ebbd1bd1a5dc927
Kabrel <wabokabrel@gmail.com>
2025-12-07 19:16:20 +0100

    Create day-03

---
+++ day-03/pizza_delivery.py
@@ -0,0 +1,32 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What pizza do you want? S, M, or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+bill = 0
+
+S = 15
+M = 20
+L = 25
+
+if (size.upper() == "S"):
+    bill = S
+
+elif (size.upper() == "M"):
+    bill = M
+
+elif (size.upper() == "L"):
+    bill = L
+
+else:
+    print ("Invalid input, please try again!")
+
+
+if (pepperoni.upper() == "Y"):
+    bill += 2
+
+if (extra_cheese.upper() == "Y"):
+    bill += 1
+
+
+print(f"Your total bill is ${bill}.")
