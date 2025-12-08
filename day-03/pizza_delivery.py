173d61c4314e0ce38f14c213242657b91fd3b659
Kabrel <wabokabrel@gmail.com>
2025-12-08 11:47:44 +0100

    Add treasure_island.py and Update pizza_delivery.py

--- day-03/pizza_delivery.py
+++ day-03/pizza_delivery.py
@@ -1,5 +1,5 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What pizza do you want? S, M, or L: ")
+size = input("What pizza size do you want? S, M, or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
