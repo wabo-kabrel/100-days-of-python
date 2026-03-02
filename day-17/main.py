1eab15197184a3d896ce1f0167167a1d6553fa80
Kabrel <wabokabrel@gmail.com>
2026-03-02 15:31:17 +0100

    Update day 17

--- day-17/main.py
+++ day-17/main.py
@@ -14,4 +14,8 @@ for i in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz.\nYour final score is: {quiz.score}/{len(quiz.questions_list)}")
